d7c653e5ad15bdb97b1a7cf766f49cb0eb1fa316
Atakan Ürey <atakanurey@gmail.com>
2022-07-07 17:52:05 +0300

    Prefix

--- index.js
+++ index.js
@@ -470,20 +470,6 @@ io.on("connection", (socket) => {
       driver: data.userPhone,
     };
 
-    const userLocationdata = await insLocation.findOne({
-      driver: {
-        phone: data.userPhone,
-      },
-    });
-    console.log(userLocationdata);
-
-    let user = await userControl(null, userLocationdata.userPhone);
-
-    await createLog({
-      customer: user._id,
-      action: "trip-started",
-    });
-
     insLocation.findOneAndDelete(
       {
         driver: {
