2411cdc46553989ae837cdf02243ba7663eaa43b
Atakan Ürey <atakanurey@gmail.com>
2022-07-19 11:25:36 +0300

    .

--- controllers/website/campaignsControllers.js
+++ controllers/website/campaignsControllers.js
@@ -1,6 +1,7 @@
 const { create, list, activeCampaignlist } = require("../../services/website/campaignService");
 
-module.exports.newCampaign = (req, res) => {
+module.exports.newCampaign = async (req, res) => {
+  if (activeCampaignlist()) return res.status(400).json({ message: "There is already an active campaign" });
   create(req.body)
     .then((result) => {
       res.status(200).json(result);
@@ -20,7 +21,7 @@ module.exports.campaignlist = (req, res) => {
     });
 };
 
-module.exports.activeCampaigns = (req, res) => {
+module.exports.activeCampaign = (req, res) => {
   activeCampaignlist()
     .then((response) => {
       res.status(200).json(response);
--- routes/website/webUsersRoutes.js
+++ routes/website/webUsersRoutes.js
@@ -1,4 +1,4 @@
-const { newCampaign, campaignlist, activeCampaigns } = require("../../controllers/website/campaignsControllers");
+const { newCampaign, campaignlist, activeCampaign } = require("../../controllers/website/campaignsControllers");
 const { getIstatistics, logList } = require("../../controllers/website/customerLogsControllers");
 const { checkUserExist, getUsers, newUser, getCab, autocomplete, getInfos, cancelCab } = require("../../controllers/website/webUsersControllers");
 
@@ -15,7 +15,7 @@ router.route("/customer-logs-list").get(logList);
 
 router.route("/campaigns").post(newCampaign);
 router.route("/campaigns").get(campaignlist);
-router.route("/active-campaign").get(activeCampaigns);
+router.route("/active-campaign").get(activeCampaign);
 
 router.route("/autocomplete").post(autocomplete);
 
